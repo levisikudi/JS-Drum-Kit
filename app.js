5e152ec55a7f0d4bc49c075009975974cfcf10d7
Levis Sikudi <levisikudi@gmail.com>
2023-01-14 02:01:37 -0500

    audio functionality and styling update

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 window.addEventListener('keydown', e =>{
     const audio = document.querySelector(`audio[data-key='${e.key}']`)
-    if(!audio) return;
+    const key = document.querySelector(`.key[data-key='${e.key}']`)
+    if(!audio) return; // stops the function from running
+    audio.currentTime = 0; // resets the time to 0 for each time a valid key is pressed
     audio.play()
+    key.classList.add('playing')
     console.log(audio);
 })
