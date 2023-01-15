c58e5d725edb6768dbe74c1ada351044994337cc
Levis Sikudi <levisikudi@gmail.com>
2023-01-14 23:40:59 -0500

    refactor code and add comments

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-window.addEventListener('keydown', e =>{
+const keys = document.querySelectorAll('.key')
+
+//a function that plays the corresponding sound based on the data key
+const playSound = (e) =>{
     const audio = document.querySelector(`audio[data-key='${e.key}']`)
     const key = document.querySelector(`.key[data-key='${e.key}']`)
+    
     if(!audio) return; // stops the function from running
     audio.currentTime = 0; // resets the time to 0 for each time a valid key is pressed
     audio.play()
     key.classList.add('playing')
-    console.log(audio);
+    
    
-})
+}
 
-const keys = document.querySelectorAll('.key')
-console.log(keys);
+window.addEventListener('keydown', playSound)
 
-keys.forEach(el => {
-    el.addEventListener('transitionend', e =>{
-        el.classList.remove('playing')
+
+
+keys.forEach(el => {  //accesses each item on the NodeList
+    //add event listener that listens for transition end property
+    el.addEventListener('transitionend', e =>{ 
+        el.classList.remove('playing') //removes the playing class
     })   
 });
-
-// keys.addEventListener('transitionend', e ={
-    
-// })
